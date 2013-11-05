3feb0b11d4816c64e45059f1f1505e074254e24f
Jun Wang <junwang4@gmail.com>
2013-11-05 13:15:22 -0500

    load taxonomy csv files into database

--- code/database_admin.php
+++ code/database_admin.php
@@ -4,12 +4,14 @@ $mysqli = connect_database();
 
 
 $task_create_all_tables = 1; 
+$task_create_taxonomy_tables = 11; 
 $task_load_csv_files_to_database = 2; 
 $task_add_indexes_to_tables = 3; 
 
 $task = $task_create_all_tables;
-$task = $task_load_csv_files_to_database;
 $task = $task_add_indexes_to_tables;
+$task = $task_create_taxonomy_tables;
+$task = $task_load_csv_files_to_database;
 
 
 if ($task == $task_create_all_tables) 
@@ -24,6 +26,10 @@ elseif ($task == $task_add_indexes_to_tables)
 {
     add_indexes_to_tables($mysqli);
 }
+elseif ($task == $task_create_taxonomy_tables)
+{
+    create_taxonomy_tables($mysqli);
+}
 else
     print "wrong task\n";
 
@@ -34,8 +40,9 @@ function load_csv_files($mysqli)
     $conf = read_config();
     $csv_dir = $conf["csv_dir"];
 
-    $tables = array('Reference');
     $tables = array('Reference', 'Annotation', 'AnnotationReference', 'Keywords', 'Source', 'Comment', 'Organism', 'Dblink');
+    $tables = array('Reference');
+    $tables = array('taxName', 'taxNode');
     foreach ($tables as $table)
     {
         $null_clause = "";
@@ -60,7 +67,7 @@ function load_csv_files($mysqli)
 // -----------------------------------------------
 function add_indexes_to_tables($mysqli)
 {
-    $table_indexes = array("table" => "Annotation", "indexes" => array("keywordsId", "sourceId", "organismId", "commentId", "dblinkId", "accession", "definition"));
+    $table_indexes = array("table" => "Annotation", "indexes" => array("keywordsId", "sourceId", "organismId", "commentId", "dblinkId", "accession", "definition", "locus", "segment", "version"));
     $table_indexes = array("table" => "Reference", "indexes" => array("reference", "authors", "consortium", "title", "journal", "pubmed"));
 
     foreach ($table_indexes["indexes"] as $index)
@@ -82,6 +89,13 @@ function create_all_tables($mysqli)
     }
 }
 
+function create_taxonomy_tables($mysqli)
+{
+    $tables = array('taxName', 'taxNode');
+    foreach ($tables as $table) {
+        create_table($mysqli, $table);
+    }
+}
 
 function create_table($mysqli, $table) 
 {
@@ -127,6 +141,9 @@ function create_table($mysqli, $table)
             INDEX (dblinkId),
             INDEX (accession),
             INDEX (definition),
+            INDEX (locus),
+            INDEX (segment),
+            INDEX (version),
             PRIMARY KEY (gi)
         )";
     }
@@ -178,6 +195,41 @@ function create_table($mysqli, $table)
             PRIMARY KEY(id)
         )";
     }
+
+    // taxonomy
+    else if ($table == "taxName") {
+        $sql = "CREATE TABLE IF NOT EXISTS " . $table . " (
+            tax_id INT,
+            name VARCHAR(200),
+            INDEX (name),
+            PRIMARY KEY(tax_id)
+        )";
+    }
+    else if ($table == "taxNode") {
+        $sql = "CREATE TABLE IF NOT EXISTS " . $table . " (
+            tax_id INT,
+            parent_tax_id INT,
+            rank VARCHAR(100),
+            locus_prefix CHAR(2),
+            division_id INT,
+            inherited_div_flag INT,
+            genetic_code_id INT,
+            inherited_GC_flag INT,
+            mitochondrial_genetic_code_id INT,
+            inherited_MGC_flag INT,
+            GenBank_hidden_flag INT,
+            hidden_subtree_root_flag INT,
+            comments VARCHAR(1000),
+            INDEX (tax_id),
+            INDEX (parent_tax_id),
+            INDEX (rank)
+        )";
+            //keyId INT,
+            //PRIMARY KEY(keyId),
+            //FOREIGN KEY (tax_id) REFERENCES taxName(tax_id),
+            //FOREIGN KEY (parent_tax_id) REFERENCES taxName(tax_id),
+    }
+    
 
     if ($mysqli->query($sql) === TRUE) {
         echo "Table \"" . $table . "\" successfully created.\n";
